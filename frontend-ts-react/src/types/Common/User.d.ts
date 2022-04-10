3388d0e970e7e348fc57fba124307653fcd27e6f
mvorotyn <mrzban@gmail.com>
2022-04-10 11:56:50 +0300

    cleanup

--- frontend-ts-react/src/types/Common/User.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Capricorncd.
- * https://github.com/capricorncd
- * Date: 2022/03/27 19:05:25 (GMT+0900)
- */
-export interface UserInfo {
-  username: string
-  userId: number
-}
--- frontend-ts-react/src/types/index.d.ts
+++ frontend-ts-react/src/types/index.d.ts
@@ -1,29 +1,7 @@
-/**
- * Created by Capricorncd.
- * https://github.com/capricorncd
- * Date: 2021-06-12 22:24 (GMT+0900)
- */
-import React from 'react'
-import store from '@/stores'
-import { SxProps } from '@mui/system'
-import { Theme } from '@mui/material/styles'
-
-export * from './Common'
-export * from './Sign'
-
 export interface AnyObject {
   [key: string]: any
 }
 
-export type ClickFunction = (e?: React.MouseEvent) => void
+// export type ClickFunction = (e?: React.MouseEvent) => void
 
-export interface DefaultProps {
-  className?: string
-  onClick?: ClickFunction
-  children?: JSX.Element | React.ReactNode
-  style?: AnyObject
-  data?: AnyObject | AnyObject[] | null
-  sx?: SxProps<Theme>
-}
 
-export type RootState = ReturnType<typeof store.getState>
